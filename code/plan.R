b7bd51156b862070a5a9192e1f084eb5a3afefb4
Grant McDermott <grant.mcdermott@gmail.com>
2020-07-21 21:47:54 -0700

    Need to sum again in certain cases bc of TZ overlap

--- code/plan.R
+++ code/plan.R
@@ -13,12 +13,11 @@ plan =
     
     ## Get daily 2020 Seattle data (to date)
     sea2020janjun = rbindlist(
-      lapply(
-        1:6,
-        function(m) get_gh_pushes(year = 2020, month = m,
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles'))
-      ),
+      lapply(1:6,
+             function(m) get_gh_pushes(year = 2020, month = m,
+                                       city = 'Seattle', state = 'WA',
+                                       tz = 'America/Los_Angeles'))
+      )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
     
     ## Get 2019 Seattle data
     sea2019 = rbind(
@@ -28,7 +27,7 @@ plan =
       as.data.table(get_gh_pushes(year = 2019, 
                                   city = 'Seattle', state = 'WA',
                                   tz = 'America/Los_Angeles'))[year(date) != 2020]
-      ),
+      )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
     
     ## Plot the difference between the global 2019 and 2020 data
     sea_daily_diff_plot = daily_diff_plot(rbind(sea2019, sea2020janjun), '2020-01-02', '2020-06-30'),
