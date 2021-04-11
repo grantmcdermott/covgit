51fd20e97d13de2c06abfee829ad765ae4be7269
Grant McDermott <grant.mcdermott@gmail.com>
2021-04-10 22:54:48 -0700

    Update plan
    - Match new functions
    - New queries for countries, SFO, and SEA (LinkedIn-matched).

--- code/plan.R
+++ code/plan.R
@@ -1,106 +1,295 @@
 plan =
   drake_plan(
+    
+    ## Countries (summarised by year)
+    c19_summ = rbind(
+      as.data.table(get_gh_activity_country(billing, year = 2019, month = 1)),
+      as.data.table(get_gh_activity_country(billing, year = 2019))
+      )[, lapply(.SD, sum), by = .(country_code)][order(-users)] %>%
+      .[, year := 2019],
+    
+    c20_summ = rbind(
+      as.data.table(get_gh_activity_country(billing, year = 2020, month = 1)),
+      as.data.table(get_gh_activity_country(billing, year = 2020))
+      )[, lapply(.SD, sum), by = .(country_code)][order(-users)] %>%
+      .[, year := 2020],
+    
+    c_all_sum = rbind(c19_summ, c20_summ),
+    
+    ## Country by date
+    c19 = rbind(
+      as.data.table(get_gh_activity_country(
+        billing, year = 2019, month = 1, excl_nulls = TRUE, by_date = TRUE
+        )),
+      as.data.table(get_gh_activity_country(
+        billing, year = 2019, excl_nulls = TRUE, by_date = TRUE))
+      ),
+    
+    c20 = rbind(
+      as.data.table(get_gh_activity_country(
+        billing, year = 2020, month = 1, excl_nulls = TRUE, by_date = TRUE
+      )),
+      as.data.table(get_gh_activity_country(
+        billing, year = 2020, excl_nulls = TRUE, by_date = TRUE))
+      ),
+    
+    c_all = rbind(c19, c20),
+    
+    fwrite(c_all, here('data/countries-all.csv')),
 
 # Global ------------------------------------------------------------------
 
-    ## Get 2021 global data (to date)
-    seq21 = setdiff(1:(which(month.abb==months(Sys.Date(), abbreviate = TRUE))-1),
-                    0), 
+    ## Get 2020 global event data
+    g20 = rbind(
+      as.data.table(get_gh_activity(billing, year = 2020, month = 1)), 
+      as.data.table(get_gh_activity(billing, year = 2020))
+      ),
     
-    g21 = rbindlist(lapply(seq21, function(m) get_gh_pushes(year = 2021, month = m))),
-    
-    ## Get 2020 global data
-    g20 = rbind(as.data.table(get_gh_pushes(year = 2020, month = 1)), 
-                as.data.table(get_gh_pushes(year = 2020))[year(date) != 2021]),
-    
-    ## Get 2019 global data
-    g19 = rbind(as.data.table(get_gh_pushes(year = 2019, month = 1)), 
-                as.data.table(get_gh_pushes(year = 2019))[year(date) != 2020]),
+    ## Get 2019 global event data
+    g19 = rbind(
+      as.data.table(get_gh_activity(billing, year = 2019, month = 1)), 
+      as.data.table(get_gh_activity(billing, year = 2019))
+      ),
     
     ## Join global data tables
-    g = rbind(g19, g20, g21),
+    g = rbind(g19, g20),
     
     ## Write to disk
-    fwrite_global = fwrite(g, here('data/global.csv')),
+    fwrite(g, here('data/global.csv')),
     
-    ## Plot the difference between the global 2019 and 2020 data
-    g_diff_plot = daily_diff_plot(g, '2020-01-02', '2020-12-31'),
+    ## Plot the difference between the early 2019 and 2020 global data.
+    ## Note that we start in mid-Feb to avoid the weird bump in user activity
+    ## that occured in early Feb 2019.
+    g_diff_plot_events = daily_diff_plot(
+      g, y = 'events', start_date = '2020-02-15', end_date = '2020-05-31'
+      ),
+    g_diff_plot_users = daily_diff_plot(
+      g, y = 'users', start_date = '2020-02-15', end_date = '2020-05-31'
+      ),
 
 
-# Seattle -----------------------------------------------------------------
+# San Francisco ------------------------------------------------------------
 
-    ## Get daily 2021 Seattle data (to date)
-    sea21 = rbindlist(
-      lapply(seq21,
-             function(m) get_gh_pushes(year = 2021, month = m,
-                                       city = 'Seattle', state = 'WA',
-                                       tz = 'America/Los_Angeles'))
-      )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
+    ## Get 2019 Seattle data
+    sfo19 = rbind(
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2019, month = 1, 
+        city = 'San Francisco', state = 'CA',
+        tz = 'America/Los_Angeles'
+      )), 
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2019, 
+        city = 'San Francisco', state = 'CA',
+        tz = 'America/Los_Angeles'
+      ))
+    )[, 
+      .(events = sum(events), users = max(users)), 
+      by = .(date, location, users_tab, event_type)], ## Need to aggregate Jan 31st TZ overlaps
     
     ## Get 2020 Seattle data
-    sea20 = rbind(
-      as.data.table(get_gh_pushes(year = 2020, month = 1, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles')), 
-      as.data.table(get_gh_pushes(year = 2020, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles'))[year(date) != 2021]
-    )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
+    sfo20 = rbind(
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2020, month = 1, 
+        city = 'San Francisco', state = 'CA',
+        tz = 'America/Los_Angeles'
+      )), 
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2020, 
+        city = 'San Francisco', state = 'CA',
+        tz = 'America/Los_Angeles'
+      ))
+    )[, 
+      .(events = sum(events), users = max(users)), 
+      by = .(date, location, users_tab, event_type)], ## Need to aggregate Jan 31st TZ overlaps
+    
+    ## Join San Francisco data tables
+    sfo = rbind(sfo19, sfo20),
+    
+    ## Write to disk
+    fwrite(sfo, here('data/sfo.csv')),
+    
+    ## Plot the difference between the early 2019 and 2020 San Francisco data
+    sfo_diff_plot_events = daily_diff_plot(
+      sea, y = 'events', start_date = '2020-01-02', end_date = '2020-05-31'
+      ),
+    sfo_diff_plot_users = daily_diff_plot(
+      sea, y = 'users', start_date = '2020-01-02', end_date = '2020-05-31'
+      ),
+
+# Seattle -----------------------------------------------------------------
     
     ## Get 2019 Seattle data
     sea19 = rbind(
-      as.data.table(get_gh_pushes(year = 2019, month = 1, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles')), 
-      as.data.table(get_gh_pushes(year = 2019, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles'))[year(date) != 2020]
-      )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2019, month = 1, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles'
+      )), 
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2019, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles'
+      ))
+    )[, 
+      .(events = sum(events), users = max(users)), 
+      by = .(date, location, users_tab, event_type)], ## Need to aggregate Jan 31st TZ overlaps
+
+    ## Get 2020 Seattle data
+    sea20 = rbind(
+      as.data.table(get_gh_activity(
+          billing,
+          year = 2020, month = 1, 
+          city = 'Seattle', state = 'WA',
+          tz = 'America/Los_Angeles'
+          )), 
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2020, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles'
+        ))
+      )[, 
+        .(events = sum(events), users = max(users)), 
+        by = .(date, location, users_tab, event_type)], ## Need to aggregate Jan 31st TZ overlaps
     
     ## Join Seattle data tables
-    sea = rbind(sea19, sea20, sea21),
+    sea = rbind(sea19, sea20),
     
     ## Write to disk
-    fwrite_sea = fwrite(sea, here('data/sea.csv')),
+    fwrite(sea, here('data/sea.csv')),
     
-    ## Plot the difference between the Seattle 2019 and 2020 data
-    sea_diff_plot = daily_diff_plot(sea, '2020-01-02', '2020-12-31'),
-
+    ## Plot the difference between the early 2019 and 2020 Seattle data
+    sea_diff_plot_events = daily_diff_plot(
+      sea, y = 'events', start_date = '2020-01-02', end_date = '2020-05-31'
+      ),
+    sea_diff_plot_users = daily_diff_plot(
+      sea, y = 'users', start_date = '2020-01-02', end_date = '2020-05-31'
+      ),
+    
 
 # Seattle (Jan 2019 cohort) -----------------------------------------------
 
     ## Same as the above, but this time limited to the group of users who were 
     ## active during January 2019. In other words, we follow the exact same 
     ## users through and try to isolate the intensive margin for this cohort.
-    sea20_cohort = rbind(
-      as.data.table(get_gh_pushes(year = 2020, month = 1, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles',
-                                  users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901')), 
-      as.data.table(get_gh_pushes(year = 2020, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles',
-                                  users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901'))[year(date) != 2021]
-    )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
-    
     sea19_cohort = rbind(
-      as.data.table(get_gh_pushes(year = 2019, month = 1, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles',
-                                  users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901')), 
-      as.data.table(get_gh_pushes(year = 2019, 
-                                  city = 'Seattle', state = 'WA',
-                                  tz = 'America/Los_Angeles',
-                                  users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901'))[year(date) != 2020]
-    )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2019, month = 1, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles',
+        users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901'
+        )), 
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2019, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles',
+        users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901'
+        ))
+      )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
+
+    sea20_cohort = rbind(
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2020, month = 1, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles',
+        users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901'
+        )), 
+      as.data.table(get_gh_activity(
+        billing,
+        year = 2020, 
+        city = 'Seattle', state = 'WA',
+        tz = 'America/Los_Angeles',
+        users_tab = 'mcd-lab.covgit.sea_users_ght1906_matched_gharch201901'
+        ))
+      )[, .(pushes = sum(pushes)), by = .(date, location)], ## Need to aggregate again b/c of TZ overlaps
     
     # Join Seattle cohort data
     sea_cohort = rbind(sea19_cohort, sea20_cohort)[, location := paste(location, 'cohort')],
     
     ## Write to disk
-    fwrite_sea_cohort = fwrite(sea, here('data/sea-cohort.csv')),
+    fwrite(sea_cohort, here('data/sea-cohort.csv')),
     
     ## Diff plot
-    sea_diff_plot_cohort = daily_diff_plot(sea_cohort, '2020-01-02', '2020-12-31')
-    
-    )
+    sea_cohort_diff_plot_events = daily_diff_plot(
+      sea_cohort, y = 'events', start_date = '2020-01-02', end_date = '2020-05-31'
+      ),
+    sea_cohort_diff_plot_users = daily_diff_plot(
+      sea_cohort, y = 'users', start_date = '2020-01-02', end_date = '2020-05-31'
+      ),
+
+
+# Seattle (matched linkedin) ----------------------------------------------
+
+  ## As above, but this time on a subset of users matched to a LinkedIn profile.
+  ## Allows us to categorise by age and gender.
+  sea19_linkedin = rbind(
+    as.data.table(get_gh_activity(
+      billing,
+      year = 2019, month = 1, 
+      # city = 'Seattle', state = 'WA',
+      tz = 'America/Los_Angeles',
+      users_tab = 'mcd-lab.covgit.sea_users_linkedin', 
+      gender = TRUE, 
+      age_buckets = c(30, 40, 50)
+    )), 
+    as.data.table(get_gh_activity(
+      billing,
+      year = 2019,
+      # city = 'Seattle', state = 'WA',
+      tz = 'America/Los_Angeles',
+      users_tab = 'mcd-lab.covgit.sea_users_linkedin', 
+      gender = TRUE, 
+      age_buckets = c(30, 40, 50)
+    ))
+  )[, 
+    .(pushes = sum(pushes), commits = sum(commits), num_users = max(num_users)), 
+    by = .(date, gender, age, users_tab)], ## Need to aggregate again b/c of a few TZ overlaps
+
+  sea20_linkedin = rbind(
+    as.data.table(get_gh_activity(
+      billing,
+      year = 2020, month = 1, 
+      # city = 'Seattle', state = 'WA',
+      tz = 'America/Los_Angeles',
+      users_tab = 'mcd-lab.covgit.sea_users_linkedin', 
+      gender = TRUE, 
+      age_buckets = c(30, 40, 50)
+      )), 
+    as.data.table(get_gh_activity(
+      billing,
+      year = 2020,
+      # city = 'Seattle', state = 'WA',
+      tz = 'America/Los_Angeles',
+      users_tab = 'mcd-lab.covgit.sea_users_linkedin', 
+      gender = TRUE, 
+      age_buckets = c(30, 40, 50)
+      ))
+    )[, 
+    .(pushes = sum(pushes), commits = sum(commits), num_users = max(num_users)), 
+    by = .(date, gender, age, users_tab)], ## Need to aggregate again b/c of a few TZ overlaps
+
+  # Join Seattle cohort data
+  sea_linkedin = rbind(sea19_linkedin, sea20_linkedin)[, location := 'seattle_linkedin'],
+  
+  ## Write to disk
+  fwrite(sea_linkedin, here('data/sea-linkedin.csv')),
+  
+  ## Diff plot
+  sea_linkedin_diff_plot_events = daily_diff_plot(
+    sea_linkedin, y = 'events', start_date = '2020-01-02', end_date = '2020-05-31'
+    ),
+  sea_linkedin_diff_plot_users = daily_diff_plot(
+    sea_linkedin, y = 'users', start_date = '2020-01-02', end_date = '2020-05-31'
+    ),
+  
+  )
+
